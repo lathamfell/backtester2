43b8a614c5c9594cb0b2c72dc2b051be4653cd85
Latham Fell <lathamfell@gmail.com>
2021-11-01 15:50:44 -0700

    add .idea to git

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-pytest --disable-pytest-warnings --cov=backtester --cov-report term-missing test.py -vv
+python3 -m pytest --disable-pytest-warnings --cov=backtester --cov-report term-missing test.py -vv
