27c0f7ec9995febcd2571e224e27418b9540df46
Latham Fell <lathamfell@gmail.com>
2022-01-11 18:57:36 -0800

    saving

--- run_scenarios.py
+++ run_scenarios.py
@@ -1,15 +1,15 @@
 from backtester import main
 import config_common as cc
 
-HTF_TPS = [[4], [6], [8]]
-LTF_TPS = [[0.3], [0.5], [1.5], [2], [3], [4]]
-LLTF_TPS = [[0.3], [0.5], [1], [1.5]]
+HTF_TPS = [[1], [2], [3], [4], [5], [6], [7]]
+LTF_TPS = [[0.25], [0.3], [0.5], [1], [2], [3], [4]]
+LLTF_TPS = [[0.15], [0.25], [0.3], [0.5], [1]]
 
-HTF_SLS = [[10]]
-LTF_SLS = [[10]]
-LLTF_SLS = [[4], [10]]
+HTF_SLS = [[9], [10], [11]]
+LTF_SLS = [[9], [10], [11]]
+LLTF_SLS = [[9], [10], [11]]
 
-HTF_DCAS = [[[[0.5, 50]]], [[[1, 50]]], [[[3, 50]]], [[[4, 50]]], [[[8, 50]]]]
+HTF_DCAS = [[[[1, 50]]], [[[2, 50]]], [[[3, 50]]], [[[4, 50]]], [[[5, 50]]], [[[6, 50]]], [[[7, 50]]], [[[8, 50]]]]
 LTF_DCAS = LLTF_DCAS = HTF_DCAS
 
 
@@ -46,6 +46,7 @@ def run_tri_arrow(configs):
         db_coll=cc.BTC_COLL,
         datafilenames=[
             "data/ready_for_backtester/BYBIT_BTCUSD_1D_45m_5m_on_5m_01_2020.csv",
+            "data/ready_for_backtester/BYBIT_ETHUSD_1D_45m_5m_on_5m_01_2020.csv"
         ],
         enable_qol=False,
         #signal_timeframes=[["1h", "5m"]],
@@ -53,10 +54,11 @@ def run_tri_arrow(configs):
         tps_after_dca=configs[0],
         stop_losses=configs[1],
         dcas=configs[2],
-        leverages=[[1, 1, 1], [3, 3, 3]],
+        leverages=[[1, 1, 1], [2, 2, 2], [3, 3, 3]],
+        #leverages=[[1, 1, 1]],
         replace_existing_scenarios=False,
-        drawdown_limits=[-100]
-        #multiproc=False
+        drawdown_limits=[-100],
+        multiproc=True
     )
 
 
